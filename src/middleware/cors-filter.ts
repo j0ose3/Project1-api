d60f6b6328df164b18b42f8f64569f9558e6565e
Jose Angeles <humblekgames@gmail.com>
2020-05-26 03:21:32 -0400

    Changed endpoints

--- src/middleware/cors-filter.ts
+++ src/middleware/cors-filter.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 export function corsFilter(req: Request, resp: Response, next) {
-  resp.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  resp.header("Access-Control-Allow-Origin", "http://revature-52606.s3-website.us-east-2.amazonaws.com");
   resp.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
   resp.header("Access-Control-Allow-Credentials", "true");
   resp.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
